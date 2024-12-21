3611d075eb544b508fd39bb22198f6f06a60c1a9
sandeep-kumar7061 <sandeep.kumar.patel7061@gmail.com>
2024-12-21 20:11:06 +0530

    code

---
+++ calculate-perimeter-of-rectangle.c
@@ -0,0 +1,21 @@
+//calculate perimeter of rectangle
+#include <stdio.h>
+
+int main() {
+    float a, b, perimeter;
+
+    // Ask the user to enter the lengths of the sides
+    printf("Enter the length of side a: ");
+    scanf("%f", &a);
+
+    printf("Enter the length of side b: ");
+    scanf("%f", &b);
+
+    // Calculate the perimeter
+    perimeter = 2 * (a + b);
+
+    // Display the perimeter
+    printf("The perimeter of the rectangle is: %.2f\n", perimeter);
+
+    return 0;
+}
